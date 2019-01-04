2388b0c185352e299c30d4be52f5d68b2653446d
obf1313 <oubaifan@163.com>
2019-01-04 14:46:48 +0800

    react-dnd done

--- src/module/diagonAlley/components/item.js
+++ src/module/diagonAlley/components/item.js
@@ -2,6 +2,11 @@ import React,{Component} from 'react'
 import {Card,Button,Row} from 'antd'
 import {toCart} from '../actions'
 import { connect } from 'react-redux'
+import {findDOMNode} from 'react-dom'
+import {
+    DragSource,
+    DropTarget,
+} from 'react-dnd'
 
 const {Meta} = Card
 const mapStateToProps = state => {
@@ -15,13 +20,66 @@ const mapDispatchToProps = dispatch => {
         }
     }
 }
+
+// 拖拽
+const Types = { // 设定类型，只有DragSource和DropTarget的类型相同时，才能完成拖拽和放置
+    CARD: 'CARD'
+}
+
+const CardSource = {
+    beginDrag(props,monitor,component){
+        return {
+            index:props.index
+        }
+    }
+}
+const CardTarget = {
+    hover(props,monitor,component){ //组件在target上方时触发的事件
+        if(!component) return null;
+        const dragIndex = monitor.getItem().index;//拖拽目标的Index
+        const hoverIndex = props.index; //目标Index
+        if(dragIndex === props.lastIndex || hoverIndex === props.lastIndex) return null;
+        if(dragIndex === hoverIndex) {return null}//如果拖拽目标和目标ID相同不发生变化
+        const hoverBoundingRect = (findDOMNode(component)).getBoundingClientRect();//获取卡片的边框矩形
+        const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2;//获取X轴中点
+        const clientOffset = monitor.getClientOffset();//获取拖拽目标偏移量
+        const hoverClientX = (clientOffset).x - hoverBoundingRect.left;
+        if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
+            return null
+        }
+        if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
+            return null
+        }
+        props.onDND(dragIndex,hoverIndex);
+        monitor.getItem().index = hoverIndex;
+    },
+}
+
+function targetCollect(connect,monitor) {
+    return{
+        connectDropTarget:connect.dropTarget(),
+        isOver:monitor.isOver(),
+        isOverCurrent: monitor.isOver({ shallow: true }),
+        canDrop: monitor.canDrop(),
+        itemType: monitor.getItemType(),
+    }
+}
+function sourceCollect(connect,monitor) {
+    return{
+        connectDragSource:connect.dragSource(),
+        isDragging:monitor.isDragging()
+    }
+}
+
 class ItemModal extends Component {
     render() {
-        const { toCart,item } = this.props
-        return (
+        const { toCart,item,isDragging, connectDragSource, connectDropTarget} = this.props
+        let opacity = isDragging ? 0.1 : 1
+        return connectDragSource( //使用DragSource 和 DropTarget
+            connectDropTarget(<div>
                 <Card
                     hoverable
-                    style={{ width: 300, margin: '20px 20px'}}
+                    style={{ width: 300, margin: '20px 20px',opacity}}
                     cover={<img alt="example" src={item.imgUrl} />}
                 >
                     <Meta
@@ -33,7 +91,8 @@ class ItemModal extends Component {
                         <Button size="small" type="primary">立即购买</Button>
                     </Row>
                 </Card>
-        )
+                </div>
+        ))
     }
 }
 
@@ -42,4 +101,4 @@ const Item = connect(
     mapDispatchToProps
 )(ItemModal)
 
-export default Item
+export default DropTarget(Types.CARD,CardTarget,targetCollect)(DragSource(Types.CARD,CardSource,sourceCollect)(Item))
--- src/module/diagonAlley/components/itemList.js
+++ src/module/diagonAlley/components/itemList.js
@@ -1,20 +1,49 @@
 import React,{Component} from 'react'
 import Item from './item'
+import {DragDropContext} from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
 
 var Mock = require('mockjs')
 var Random = Mock.Random
-export default class ItemList extends Component{
-    render() {
+
+class ItemList extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+            itemList: []
+        }
+    }
+
+    componentDidMount() {
         let itemList = Mock.mock({
             'list|10': [{
                 'id|+1': 1,
                 'price|1-100.2': 20,
-                'title|+1': ["魔杖","魔法袍","学习用品"],
+                'title|+1': ["魔杖一","魔法袍二","学习用品三"],
                 'imgUrl|+1':[Random.image('300x200'),Random.image('300x200')]
             }]
         })
-        let itemListHtml = itemList.list.map((item)=>(
-            <Item item={item} key={item.id}/>
+        this.setState({itemList: itemList.list})
+    }
+
+    handleDND = (dragIndex,hoverIndex) => {
+        let itemList = this.state.itemList;
+        let tmp = itemList[dragIndex] //临时储存文件
+        itemList.splice(dragIndex,1) //移除拖拽项
+        itemList.splice(hoverIndex,0,tmp) //插入放置项
+        console.log('拖拽目标的Index',dragIndex)
+        console.log('目标Index',hoverIndex)
+        this.setState({itemList})
+    }
+
+    render() {
+        let itemListHtml = this.state.itemList.map((item,index)=>(
+            <Item item={item}
+                  key={item.id}
+                  index={index}
+                  onDND={this.handleDND}
+            />
         ))
         return (
             <div style={{padding: 40, display: 'flex',flexWrap: 'wrap'}}>
@@ -23,3 +52,5 @@ export default class ItemList extends Component{
         )
     }
 }
+
+export default DragDropContext(HTML5Backend)(ItemList)
